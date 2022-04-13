6e0b216fabf12872047d29061583cadb6512b27e
AiRAM-S <2579701825@qq.com>
2022-04-13 17:00:12 +0800

    add player indicator

--- checker(1)/debug/moc_mydialog1.cpp
+++ checker(1)/debug/moc_mydialog1.cpp
@@ -7,7 +7,7 @@
 *****************************************************************************/
 
 #include <memory>
-#include "D:/QtDemo/checker(1)/mydialog1.h"
+#include "../mydialog1.h"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
